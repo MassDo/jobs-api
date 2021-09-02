2934330716fdae37e227b7075c9092c514c275c6
massdo <dorian_massoulier@hotmail.com>
2021-09-02 10:49:17 +0200

    update job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -22,7 +22,23 @@ const getJob = async (req, res) =>{
     res.status(StatusCodes.OK).json(job)
 }
 const updateJob = async (req, res) =>{
-    res.send('update job')
+    const {
+        user: {userId}, 
+        params:{id:jobId}, 
+        body:{company, position}
+    } = req
+    if(company === "" || position === ""){
+        throw new BadRequestError('You need to provide company and position infos!')
+    }
+    const job = await Job.findByIdAndUpdate(
+        { _id:jobId, createdBy:userId },
+        req.body,
+        { new:true, runValidators: true}
+    )
+    if(!job){
+        throw new NotFoundError(`No job matching this id: ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json(job)
 }
 const deleteJob = async (req, res) =>{
     res.send('delete job')
