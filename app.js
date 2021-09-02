0bb4838cd1ce75c5a25c29a592574861b96fd417
massdo <dorian_massoulier@hotmail.com>
2021-09-02 21:34:08 +0200

    add basic security package and rate limitation

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 const app = express()
-// authorization
+// import extra serity packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+// import authorization
 const authMiddleware = require('./middleware/authentication')
-// error handler
+// import error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
-// router
+// import router
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
-// database
+// import database
 const connectDB = require('./db/connect')
-
 // middleware
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter({windowMs:60 * 1000, max:60}))
+
 
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authMiddleware, jobsRouter)
-
 // errors
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
-
+// start server
 const port = process.env.PORT || 3000
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
@@ -34,5 +42,4 @@ const start = async () => {
     console.log(error)
   }
 }
-
 start()
